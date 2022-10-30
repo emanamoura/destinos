52c50a3a370929b6b97fb80c72aeb9ba8859c180
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-10-30 15:52:41 -0300

    feat: Input cpf field is now optional

--- src/components/UserForm/index.tsx
+++ src/components/UserForm/index.tsx
@@ -1,19 +1,43 @@
-import { useContext } from "react";
 import { useDataContext } from "../../context/DataContext";
-import './style.css';
+import "./style.css";
 
-export const UserForm = () => {
-  const {setName, setEmail, setTelefone, setCpf } = useDataContext();
+interface UserFormProps {
+  showInputCpf?: boolean;
+}
+
+export const UserForm = ({ showInputCpf = true }: UserFormProps) => {
+  const { setName, setEmail, setTelefone, setCpf } = useDataContext();
   return (
     <form className="form">
-      <label htmlFor="name">Name</label>
-      <input placeholder="Ex: Emanuel Moura" name="name" onChange={e => setName(e.target.value)}/>
-      <label htmlFor="email">Email</label>
-      <input placeholder="Ex: emanuel.moura@ccc.ufcg.edu.br" name="email" onChange={e => setEmail(e.target.value)} />
-      <label htmlFor="telefone">Telefone</label>
-      <input placeholder="Ex: (83) 9 8164-1347" name="telefone" onChange={e => setTelefone(e.target.value)} />
-      <label htmlFor="cpf">CPF</label>
-      <input placeholder="Ex: 999.999.999-15" name="cpf" onChange={e => setCpf(e.target.value)} />
+      <h2>dados Pessoais</h2>
+      <label htmlFor="name">nome</label>
+      <input
+        placeholder="Ex: Emanuel Moura"
+        name="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="email">email</label>
+      <input
+        placeholder="Ex: emanuel.moura@ccc.ufcg.edu.br"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="telefone">telefone</label>
+      <input
+        placeholder="Ex: (83) 9 8164-1347"
+        name="telefone"
+        onChange={(e) => setTelefone(e.target.value)}
+      />
+      {showInputCpf && (
+        <>
+          <label htmlFor="cpf">cpf</label>
+          <input
+            placeholder="Ex: 999.999.999-15"
+            name="cpf"
+            onChange={(e) => setCpf(e.target.value)}
+          />
+        </>
+      )}
     </form>
   );
 };
